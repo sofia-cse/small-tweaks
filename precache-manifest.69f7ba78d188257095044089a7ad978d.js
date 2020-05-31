c0da8dc36687d07545689c71a0e44fd75e328592
Joseph Martin <josephmartinux@gmail.com>
2020-05-30 22:52:30 -0700

    Updates

---
+++ precache-manifest.69f7ba78d188257095044089a7ad978d.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a909bd193b8f8b25cacd99935ba1a3",
+    "url": "/small-tweaks/index.html"
+  },
+  {
+    "revision": "769bb62fff9f610e62e5",
+    "url": "/small-tweaks/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "97129aaf862b6cbe42c4",
+    "url": "/small-tweaks/static/css/main.c475929c.chunk.css"
+  },
+  {
+    "revision": "769bb62fff9f610e62e5",
+    "url": "/small-tweaks/static/js/2.5b35737c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/small-tweaks/static/js/2.5b35737c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97129aaf862b6cbe42c4",
+    "url": "/small-tweaks/static/js/main.b547ce2a.chunk.js"
+  },
+  {
+    "revision": "f9112e47d36321674eb5",
+    "url": "/small-tweaks/static/js/runtime-main.92d15932.js"
+  },
+  {
+    "revision": "3121886f7957dfd38ea318e19db58784",
+    "url": "/small-tweaks/static/media/Akkurat-Bold-Italic.3121886f.woff"
+  },
+  {
+    "revision": "da8a55f6177ce36349693d1e1af584cf",
+    "url": "/small-tweaks/static/media/Akkurat-Bold-Italic.da8a55f6.eot"
+  },
+  {
+    "revision": "e8e24f279005cbc3a13ae2e794e35de2",
+    "url": "/small-tweaks/static/media/Akkurat-Bold-Italic.e8e24f27.ttf"
+  },
+  {
+    "revision": "5e8d68ff9f401095a08cb8da6ecc4eac",
+    "url": "/small-tweaks/static/media/Akkurat-Bold.5e8d68ff.woff"
+  },
+  {
+    "revision": "aab835cc05970addb042cf3446e15288",
+    "url": "/small-tweaks/static/media/Akkurat-Bold.aab835cc.eot"
+  },
+  {
+    "revision": "c2b033098c6e497d5d2651c0e74c246c",
+    "url": "/small-tweaks/static/media/Akkurat-Bold.c2b03309.ttf"
+  },
+  {
+    "revision": "a4e7e48dc54ced75ec84e61ff327244e",
+    "url": "/small-tweaks/static/media/Akkurat-Normal-Italic.a4e7e48d.ttf"
+  },
+  {
+    "revision": "b40ea839d542282bf8102335b9491312",
+    "url": "/small-tweaks/static/media/Akkurat-Normal-Italic.b40ea839.woff"
+  },
+  {
+    "revision": "c5c5d696fc17b16ec08786c55954603c",
+    "url": "/small-tweaks/static/media/Akkurat-Normal-Italic.c5c5d696.eot"
+  },
+  {
+    "revision": "1f4707686e524e2f79bc5360fe6db13a",
+    "url": "/small-tweaks/static/media/Akkurat-Normal.1f470768.woff"
+  },
+  {
+    "revision": "7540cfc45df751bbaebbd169b781297e",
+    "url": "/small-tweaks/static/media/Akkurat-Normal.7540cfc4.ttf"
+  },
+  {
+    "revision": "b750c9df60eeda3cbb66595d2508d56b",
+    "url": "/small-tweaks/static/media/Akkurat-Normal.b750c9df.eot"
+  },
+  {
+    "revision": "518d1d74ece66cd3b88da2994b2fd17f",
+    "url": "/small-tweaks/static/media/avinew-comparison@2x.518d1d74.png"
+  },
+  {
+    "revision": "55c55e4c81917039e7bdf21e5a9d45ea",
+    "url": "/small-tweaks/static/media/avinew-logo@2x.55c55e4c.png"
+  },
+  {
+    "revision": "3065b5859df3dc186ac5d5d193c49cfc",
+    "url": "/small-tweaks/static/media/avinew-quote@2x.3065b585.png"
+  },
+  {
+    "revision": "3d615e37ee1beff51d26e31d6b0df23e",
+    "url": "/small-tweaks/static/media/fireside-comparison@2x.3d615e37.png"
+  },
+  {
+    "revision": "ec0ed5b7a8cb13f7dd13378397ee446b",
+    "url": "/small-tweaks/static/media/fireside-quote@2x.ec0ed5b7.png"
+  },
+  {
+    "revision": "82f270139cf12746ccebb78a96c2b0b3",
+    "url": "/small-tweaks/static/media/fireside@2x.82f27013.png"
+  },
+  {
+    "revision": "701145c91930b095a0e1452a1e774032",
+    "url": "/small-tweaks/static/media/hero-graphic@2x.701145c9.png"
+  },
+  {
+    "revision": "41112c83af836a3d6ad43e7b1851e75c",
+    "url": "/small-tweaks/static/media/implement-illustration@2x.41112c83.png"
+  },
+  {
+    "revision": "20e34e4dc427de567cbb7087f4786ee5",
+    "url": "/small-tweaks/static/media/logo.20e34e4d.svg"
+  },
+  {
+    "revision": "181f794b0622f42188d0baad95a2f185",
+    "url": "/small-tweaks/static/media/n2uitive-comparison@2x.181f794b.png"
+  },
+  {
+    "revision": "168741812427a7b19468bcbf6910cbd8",
+    "url": "/small-tweaks/static/media/n2uitive-quote@2x.16874181.png"
+  },
+  {
+    "revision": "8230269c714416b40bacfd6ac57d369a",
+    "url": "/small-tweaks/static/media/n2uitive@2x.8230269c.png"
+  },
+  {
+    "revision": "31f3c284e22ef1f2b77e7e579fac51c8",
+    "url": "/small-tweaks/static/media/prototype-illustration@2x.31f3c284.png"
+  },
+  {
+    "revision": "82ea1af68da3817730cb01034ee7ccfd",
+    "url": "/small-tweaks/static/media/research-illustration@2x.82ea1af6.png"
+  },
+  {
+    "revision": "8022c60da821e68c5830a674fd20ac16",
+    "url": "/small-tweaks/static/media/satellite-illustration@2x.8022c60d.png"
+  },
+  {
+    "revision": "b7798085745a5e519f7e17ff4f500f79",
+    "url": "/small-tweaks/static/media/sears-illustration@2x.b7798085.png"
+  },
+  {
+    "revision": "93d941c362c11a16668cdc6d000548f4",
+    "url": "/small-tweaks/static/media/shades-of-blue-illustration@2x.93d941c3.png"
+  }
+]);
